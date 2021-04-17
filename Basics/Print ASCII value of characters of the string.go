ef154053b1779ecbef0f61c3bdb703bc6431bad6
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-04-17 15:53:20 +0530

    Update Print ASCII value of characters of the string.go

--- Basics/Print ASCII value of characters of the string.go
+++ Basics/Print ASCII value of characters of the string.go
@@ -11,3 +11,22 @@ func main() {
 		fmt.Printf("%c => %d\n", str[i], str[i])
 	}
 }
+
+// Method 2 
+ 
+package main
+import "fmt"
+func main(){
+	lowercase := "abcdefghijklmnopqrstunwxyz"
+	for _,c := range lowercase{
+		fmt.Println(c)
+		}
+	uppercase := "ABCDEFGHIJKLMNOPQRSTUNWXYZ"
+	for _,c := range uppercase{
+		fmt.Println(c)
+		}
+	numbers := "0123456789"
+	for _,n := range numbers {
+		fmt.Println(n)
+	}
+}
