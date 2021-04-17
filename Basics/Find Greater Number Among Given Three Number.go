3b7df09e61915ce3ee6ebf50eec2431399161a37
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-04-17 18:54:48 +0530

    Update Find Greater Number Among Given Three Number.go

--- Basics/Find Greater Number Among Given Three Number.go
+++ Basics/Find Greater Number Among Given Three Number.go
@@ -18,3 +18,22 @@ func main(){
     }
   }
 }
+
+// Method 2 
+
+package main
+import "fmt"
+func main(){
+  var num1,num2,num3 float64
+  fmt.Printf("Enter three numbers:\t")
+  fmt.Scanf("%f%f%f",&num1,&num2,&num3)
+  if(num1>num2 && num1>num3){
+    fmt.Printf("\nGreatest Number is:%.2f\t",num1)
+  }else if(num2>num3 && num2>num1){
+    fmt.Printf("\nGreatest Number is:%.2f\t",num2)
+  } else if(num1==num2 && num2==num3){
+    fmt.Printf("\nAll are equal")
+  }else{
+    fmt.Printf("\nGreatest Number is:%.2f\t",num3)
+  }
+}
