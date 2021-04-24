a15eaf64c99107ecac27e5ef24dae633c2eabaa5
Bharathi2707 <81581257+Bharathi2707@users.noreply.github.com>
2021-04-24 10:43:04 +0530

    created reverse array in go

---
+++ array/reverse an array.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+  var b int
+  var a=4
+  var s []int
+  fmt.Println("Enter the length of the array")
+  fmt.Scan(&a)
+  fmt.Println("Enter the elements for the array")
+   
+  for i:=0;i<a;i++{
+     fmt.Scan(&b)
+    s = append(s, b)
+  
+  }
+    fmt.Println("original array is ",s)
+   for i:=0;i<len(s)/2;i++{
+      j:=len(s)-i-1
+      s[i],s[j]=s[j],s[i]
+    }
+
+  fmt.Println("Reversed array is",s)
+}
