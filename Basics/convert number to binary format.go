df2060f53eaab8b4605e0854415010b55db507bf
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-04-26 16:07:36 +0530

    Update convert number to binary format.go

--- Basics/convert number to binary format.go
+++ Basics/convert number to binary format.go
@@ -5,4 +5,6 @@ func main(){
     fmt.Print("Enter number here: ")
     fmt.Scanf("%d",&n)
     fmt.Printf("Binary value of %d is: %b\n",n,n)
+    fmt.Printf("Octal value of %d is: %o\n",n,n)
+    fmt.Printf("Hexa value of %d is: %X\n",n,n)
 }
