13faf731daa5b0fb2661bd657dd935d4ef7df9e1
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-04-27 13:44:11 +0530

    Create switch without default statement, multiple values in case statement.go

---
+++ Switch/switch without default statement, multiple values in case statement.go
@@ -0,0 +1,18 @@
+package main
+  
+import "fmt"
+  
+func main() {
+    var value string = "five"
+      
+    // Switch statement without default statement
+    // Multiple values in case statement
+   switch value {
+       case "one":
+       fmt.Println("C#")
+       case "two", "three":
+       fmt.Println("Go")
+       case "four", "five", "six":
+       fmt.Println("Java")
+   }  
+}
