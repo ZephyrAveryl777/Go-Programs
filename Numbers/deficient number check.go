c697cd3001874ca5f896e9a0d96f9f78a39456a6
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-04-27 18:07:11 +0530

    Create deficient number check.go

---
+++ Numbers/deficient number check.go
@@ -0,0 +1,34 @@
+package main
+import (
+    "fmt"
+    "math"
+)
+func  divsum(n int)int{
+    var i int
+    var sum int = 0
+    var c int = int(math.Sqrt(int(n)))
+    for i:=1;i<=c;i++{
+        if(n%i == 0){
+            if((n/i)==i){
+                sum = sum+i
+            }else{
+                sum = sum+i
+                sum = sum+(n/i)
+            }
+        }
+    }
+    return sum
+}
+func isDef(n int) int{
+    return (divsum(n)<(2*n))
+}
+func main(){
+    var n int
+    fmt.Print("Enter a number: ")
+    fmt.Scanf("%d",&n)
+    if(isDef(n)==0){
+        fmt.Print("Number is  deficient.")
+    }else{
+        fmt.Print("Number is not deficient")
+    }
+}
