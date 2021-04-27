822d13fe64839c004fb07323baf84902722ce596
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-04-27 11:19:19 +0530

    Update Bitwise Operations.go

--- Basics/Bitwise Operations.go
+++ Basics/Bitwise Operations.go
@@ -13,3 +13,27 @@ func main(){
     fmt.Printf("Original Number is %d\nBinary of Original Number %d is %b\nResult of Left shift is %d\nBinary of result is %b\n",n,n,n,res,res)
 }
 // RIGHT SHIFT
+package main
+import "fmt"
+func main(){
+    var l,num,res int  
+    fmt.Printf("Enter number: ")
+    fmt.Scanf("%d",&num)
+    fmt.Printf("Enter value by which to be shifted: ")
+    fmt.Scanf("%d",&l)
+    res = num>>l
+    fmt.Printf("Original Number is %d\nBinary of Original Number %d is %b\nResult of Right shift is %d\nBinary of result is %b\n",num,num,num,res,res)
+}
+
+// BINARY AND 
+package main
+import "fmt"
+func main(){
+    var num1,num2,res int  
+    fmt.Printf("Enter number1: ")
+    fmt.Scanf("%d",&num1)
+    fmt.Printf("Enter number2: ")
+    fmt.Scanf("%d",&num2)
+    res = num1 & num2 
+    fmt.Printf("First number is: %d\nBinay of First Number %d is: %b\nSecond Number is: %d\nBinary of Second Number is: %b\nResult of bitwise and between %d and %d is: %d\nBinary of the result is:%b\n",num1,num1,num1,num2,num2,num1,num2,res,res)
+}
