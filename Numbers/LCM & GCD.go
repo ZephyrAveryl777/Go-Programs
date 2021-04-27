b3d51def9e20a11ceeffadb46311d592bf964cd6
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-04-27 09:45:29 +0530

    Create LCM & GCD.go

---
+++ Numbers/LCM & GCD.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(LCM(10, 15))
+	fmt.Println(LCM(10, 15, 20))
+	fmt.Println(LCM(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+}
