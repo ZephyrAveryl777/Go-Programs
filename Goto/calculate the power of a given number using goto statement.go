772ddb3eab87656e6fa9c4cfa471d63df0690403
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-05-01 17:46:44 +0530

    Update calculate the power of a given number using goto statement.go

--- Goto/calculate the power of a given number using goto statement.go
+++ Goto/calculate the power of a given number using goto statement.go
@@ -1,1 +1,30 @@
+// calculate the power of a 
+// given number using the goto statement
+
+package main
+
+import "fmt"
+
+func main() {
+	var num,power int
+	var result int = 1
+
+	fmt.Print("Enter Number: ")
+	fmt.Scanf("%d", &num)
+	var t int = num
+
+	fmt.Print("Enter Power: ")
+	fmt.Scanf("%d", &power)
+	var t2 int = power
+XYZ:
+	result = result * num
+
+	power = power - 1
+
+	if power >= 1 {
+		goto XYZ
+	}
+	
+	fmt.Printf("Result of %d to the power of %d (%d^%d) is: %d",t,t2,t,t2,result)
+}
 
