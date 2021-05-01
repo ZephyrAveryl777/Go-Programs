5c85a3bc62dfa8718f356b8c0e52f4b757cf062e
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-05-01 11:07:15 +0530

    Update Sum of digits.go

--- Numbers/Sum of digits.go
+++ Numbers/Sum of digits.go
@@ -12,3 +12,17 @@ func main(){
     }
     fmt.Printf("sum of digits of %d is: %d\n",n,sum)
 }
+
+// **************** Method 2 *************************
+package main 
+import "fmt"
+func main(){
+    var n,sum,remainder int 
+    fmt.Print("Enter a number: ")
+    for  fmt.Scanf("%d",&n);n!=0;n=n/10{
+        remainder = n%10
+        sum = sum+remainder 
+    }
+    fmt.Printf("Sum of digits is: %d",sum)
+}
+
