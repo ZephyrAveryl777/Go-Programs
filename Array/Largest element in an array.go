565f3b8365bf47d973c6465560aef79490dafa7e
Bharathi2707 <81581257+Bharathi2707@users.noreply.github.com>
2021-05-01 14:45:57 +0530

    Create Largest element in an array.go

---
+++ Array/Largest element in an array.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+var e,s,max int
+var r,l []int
+func evenpos(){
+  //through the array storing the maximum value and comparing it with upcoiming elements and finally printing the largest value strored in max
+  for i:=0;i<s;i=i+1 {
+    if max<l[i]{
+      max=l[i]
+    }
+  }
+fmt.Println("Largest Element is ",max)
+}
+func main() {
+fmt.Print("Enter the size of the array:")
+fmt.Scan(&s)
+fmt.Println("Enter the elements of the array:")
+for i:=0;i<s;i++{
+  fmt.Scan(&e)
+  l=append(l,e)
+}
+evenpos()
+}
