25a2e42a647be854dce37ea5ff1a38f11c134f87
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-05-01 17:55:37 +0530

    Update check the given number is palindrome or not using goto statement.go

--- Goto/check the given number is palindrome or not using goto statement.go
+++ Goto/check the given number is palindrome or not using goto statement.go
@@ -1,1 +1,24 @@
+package main 
+import "fmt"
+func main(){
+    var num,rev,rem,temp int 
+    fmt.Print("Enter Number: ")
+	fmt.Scanf("%d", &num)
 
+	temp = num
+A:
+
+	rem = num % 10
+	rev = rev*10 + rem
+	num = num / 10
+
+	if num > 0 {
+		goto A
+	}
+
+	if temp == rev {
+		fmt.Printf("Number %d, is a palindrome",temp)
+	} else {
+		fmt.Printf("Number %d, is not a palindrome",temp)
+	}
+}
