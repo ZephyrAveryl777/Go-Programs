ce705f16e9f4b545ec02cb56e031270936082ea8
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2021-05-01 19:31:46 +0530

    Update calculate HCF.go

--- Loops/calculate HCF.go
+++ Loops/calculate HCF.go
@@ -1,1 +1,22 @@
+// program to calculate the Highest Common Factor
 
+package main
+import "fmt"
+
+func main() {
+    var num1,num2,temp int 
+    
+    fmt.Printf("Enter number1: ")
+    fmt.Scanf("%d",&num1)
+    
+    fmt.Printf("Enter number2: ")
+    fmt.Scanf("%d",&num2)
+    
+    for (num2!=0){
+        temp = num1 % num2
+        num1 = num2
+        num2 = temp
+    }
+
+    fmt.Printf("Highest Common Factor is:%d", num1)
+}
